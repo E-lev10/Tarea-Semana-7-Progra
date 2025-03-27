61dbd33b490e6edfee0f4eaafe798ee9af5c5055
Selvin Tzunún Balán <selvinjr.tzunun@gmail.com>
2025-03-27 17:10:15 -0600

    Se agregaron mas lineas de codigo

--- Intercambio de valores usando punteros/Intercambio de valores usando punteros.cpp
+++ Intercambio de valores usando punteros/Intercambio de valores usando punteros.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+
+void intercambiar(int* a, int* b) {
+    int temp = *a = ;
+    *a = *b;
+    *b = temp;
+}
+int main() {
+    int num1 = 10, num2 = 20;
+    cout << "Antes del intercambio :" << endl;
+    cout << "num1 =" << num1 << ", num2 =" << num2 << endl;
+
+    intercambiar(&num1, &num2);
+    cout << "Despues del intercambio :" << endl;
+    cout << "num1 =" << num1 << ", num2 =" << num2 << endl;
+
+    return 0;
+}
+      
