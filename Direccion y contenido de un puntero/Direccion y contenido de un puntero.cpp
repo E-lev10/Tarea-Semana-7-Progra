c4566f5aba717f7cff03d570cd20a993431e267d
E-lev10 <pasevelioyaxon40@gmail.com>
2025-03-26 23:41:24 -0600

    Terminé de competar el primer código, con sus cometarios en cada línea

--- Direccion y contenido de un puntero/Direccion y contenido de un puntero.cpp
+++ Direccion y contenido de un puntero/Direccion y contenido de un puntero.cpp
@@ -6,3 +6,10 @@ int main() {
     int numero = 10;  //Declara una variable entera y le asigna un valor
 
     int* ptr = &numero;  //Sirve para declarar un puntero a entero y almacena la dirección de 'numero'
+
+        cout << "Direccion de la variable numero: " << &numero << endl;  //Muestra la dirección de nuestro 'numero' directamente
+        cout << "Direccion almacenada en el puntero ptr: " << ptr << endl;  //Muestra la dirección almacenada en nuestro puntero
+        cout << "Valor de la variable numero usando el puntero: " << *ptr << endl;  //Muestra el valor de 'numero' que va subiendo mediante el puntero
+
+        return 0;  //Esto nos quiere decir que el programa finalizó retornando 0
+    }
