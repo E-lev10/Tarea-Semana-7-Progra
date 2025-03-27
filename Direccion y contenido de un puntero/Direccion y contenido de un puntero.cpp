c34b82cf9fe363ebad3158dcad3430d1fdf1e389
E-lev10 <pasevelioyaxon40@gmail.com>
2025-03-26 23:35:27 -0600

    Escribí unas lineas de codigo con su respectivo comentario en el primer código

--- Direccion y contenido de un puntero/Direccion y contenido de un puntero.cpp
+++ Direccion y contenido de un puntero/Direccion y contenido de un puntero.cpp
@@ -1,1 +1,8 @@
+#include <iostream>  //Incluye la biblioteca estándar de entrada y salida, esto ya lo sabemos
 
+using namespace std;  //Esto nos permite que en cada linea de codigo no estemos poniendo "std::" al final, esto lo hace automaticamente
+
+int main() {
+    int numero = 10;  //Declara una variable entera y le asigna un valor
+
+    int* ptr = &numero;  //Sirve para declarar un puntero a entero y almacena la dirección de 'numero'
