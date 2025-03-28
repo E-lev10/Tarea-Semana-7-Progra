5f805fb8391a078d5df993c4ca75cbb86ae3579c
Selvin Tzunún Balán <selvinjr.tzunun@gmail.com>
2025-03-27 22:33:29 -0600

    Se comentaron las lineas del codigo

--- Intercambio de valores usando punteros/Intercambio de valores usando punteros.cpp
+++ Intercambio de valores usando punteros/Intercambio de valores usando punteros.cpp
@@ -1,21 +1,26 @@
-#include <iostream>
+#include <iostream> // Biblioteca estándar para entrada y salida
 
-using namespace std;
+using namespace std;// Permite usar elementos del espacio de nombres estándar sin escribir std::
+
+// Función que intercambia dos valores enteros usando punteros.
+// a Puntero al primer número.
+// b Puntero al segundo número.
+
 
 void intercambiar(int* a, int* b) {
-    int temp = *a = ;
-    *a = *b;
-    *b = temp;
+    int temp = *a = ; // Guarda el valor de 'a' en una variable temporal
+    *a = *b; // Asigna el valor de 'b' a 'a'
+    *b = temp;  // Asigna el valor almacenado en 'temp' a 'b
 }
-int main() {
-    int num1 = 10, num2 = 20;
-    cout << "Antes del intercambio :" << endl;
+int main() { 
+    int num1 = 10, num2 = 20; // Se declaran dos variables enteras con valores iniciales
+    cout << "Antes del intercambio :" << endl; // Muestra los valores originales antes del intercambio
     cout << "num1 =" << num1 << ", num2 =" << num2 << endl;
 
-    intercambiar(&num1, &num2);
-    cout << "Despues del intercambio :" << endl;
+    intercambiar(&num1, &num2);  // Llama a la función intercambiar pasando las direcciones de num1 y num2
+    cout << "Despues del intercambio :" << endl;  // Muestra los valores después del intercambio
     cout << "num1 =" << num1 << ", num2 =" << num2 << endl;
 
-    return 0;
+    return 0; // Indica que el programa finalizó correctamente
 }
       
